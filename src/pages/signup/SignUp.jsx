513db49346a2e81e6bf234907201ff0e2723025a
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-02-16 21:02:31 +0500

    optimized(sign-up):

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,15 +1,16 @@
 import {images} from "../../constants"
 import SignUpForm from "../../forms/AuthForm/SignUpForm";
 import "./SignUp.scss"
+import {Link} from "react-router-dom";
 
 const SignUp = () => {
     return (
         <div className="signup-page">
             <div className="signup-page__header">
-                <div className="signup-page__brand">
+                <Link to="/" className="signup-page__brand">
                     <img src={images.logo || "/placeholder.svg"} alt="Logo" className="signup-page__logo"/>
                     <span className="signup-page__brand-name">CAPELLA</span>
-                </div>
+                </Link>
             </div>
 
             <div className="signup-page__container">
